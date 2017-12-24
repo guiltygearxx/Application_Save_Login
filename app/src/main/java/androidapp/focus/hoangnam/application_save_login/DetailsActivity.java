fa56c9932c9b38010ea6f78cb438139547e127f2
nguyenvannam19942802 <nguyenvannam19942802@gmail.com>
2017-12-24 08:32:05 +0700

    # Kh?i t?o giao di?n ?ng d?ng luu tài kho?n dang nh?p

---
+++ app/src/main/java/androidapp/focus/hoangnam/application_save_login/DetailsActivity.java
@@ -0,0 +1,17 @@
+package androidapp.focus.hoangnam.application_save_login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HoangNam on 12/24/2017.
+ */
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
